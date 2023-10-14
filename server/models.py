6ae08e2352f28c8e06878f3d350d9c8716e0a828
Jmillamena <jcmillamena@gmail.com>
2023-10-13 17:37:57 -1000

    adds models

--- server/models.py
+++ server/models.py
@@ -4,3 +4,35 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+
+class House(db.Model, SerializerMixin):
+    __tablename__ = 'houses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+
+class Wand(db.Model, SerializerMixin):
+    __tablename__ = 'wands'
+
+    id = db.Column(db.Integer, primary_key=True)
+    wood = db.Column(db.String)
+    core = db.Column(db.String)
+    length = db.Column(db.Integer)
+
+
+class Pet(db.Model, SerializerMixin):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    type = db.Column(db.String)
+
+
+class Student(db.Model, SerializerMixin):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'))
